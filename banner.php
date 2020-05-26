0e69b7a1369ab36e8953f7e307bde57229316c11
khsx-403 <57099180+khsx-403@users.noreply.github.com>
2020-05-26 18:24:35 +0700

    Add files via upload

---
+++ banner.php
@@ -0,0 +1,15 @@
+<?php
+$putih = "\e[0m";
+$merah = "\e[1;31m";
+$hijau = "\e[1;92m";
+$kuning = "\e[1;33m";
+$cyan = "\e[1;36m";
+$biru = "\e[1;34m";
+$ungu = "\e[1;35m";
+@system("clear");
+$b = "{$putih}[     {$merah} ini nama script    {$putih}]
+{$putih}[ {$hijau}CREATOR BY {$putih}: {$hijau}KHSX/AGA KATOROIK {$putih}]{$merah}={$putih}[ {$hijau}CHANNEL YT {$putih}: {$hijau}KHSX CHANEL {$putih}]
+                    {$biru}•{$putih}•{$biru}•{$putih}•{$putih}[ {$kuning}SUPPORT BY {$putih}]{$biru}•{$putih}•{$biru}•{$putih}•
+{$putih}[ {$cyan}ANDRI 09 {$putih}]{$merah}={$putih}[ {$cyan}SHARKBLOODS {$putih}]{$merah}={$putih}[ {$cyan}FREE TUTORIAL {$putih}]{$merah}={$putih}[ {$cyan}XATHOSI LANZZ {$putih}]
+{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•{$putih}•{$merah}•";
+echo $b."\n\n";
